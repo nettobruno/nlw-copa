a790861ad06d8a22acd3f8b2a9f029de79c4f309
Bruno Netto <brunonettomac@outlook.com>
2022-11-02 01:49:34 -0300

    Primeira aula

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+  return (
+    <Center flex={1} backgroundColor="gray.900">
+      <Text fontSize={24} color="white">Teste de funcionamento do app!</Text>
+    </Center>
+  )
+}
